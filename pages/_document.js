9c30cc0e111a4951704ef749bf5121d9336d97b1
Nahuel-199 <gabrielnahuel96@gmail.com>
2022-04-02 21:18:46 -0300

    finish

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document{
         return(
             <Html lang="en">
                 <Head>
-                    <meta name="description" content="Dev AT E-commerce website with Next.js"/>
+                    <meta name="description" content="Sublimaciones Fox E-commerce website"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
